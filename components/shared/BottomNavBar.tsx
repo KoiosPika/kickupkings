a885492b6edbf1543d5c2108f69f11e91ac33a8b
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-15 13:22:27 -0700

    bottom nav

--- components/shared/BottomNavBar.tsx
+++ components/shared/BottomNavBar.tsx
@@ -11,36 +11,39 @@ const BottomNavBar = ({ currentPage, setCurrentPage }: BottomNavBarProps) => {
     return (
         <nav className="fixed bottom-0 w-11/12 mb-1 max-w-[480px] bg-slate-600 flex justify-around items-center p-2 shadow-md rounded-lg">
             <button
-                className={`text-sm px-2 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'home' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
+                className={`text-sm w-1/6 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'home' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
                 onClick={() => setCurrentPage('home')}
             >
-                <Image src={currentPage === 'home' ? '/icons/Home-black.svg' : '/icons/Home-white.svg'} alt='home' height={20} width={20}/>
+                <Image src={currentPage === 'home' ? '/icons/Home-black.svg' : '/icons/Home-white.svg'} alt='home' height={20} width={20} />
                 <p className='text-xs font-bold'>Home</p>
             </button>
             <button
-                className={`text-sm px-2 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'shop' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
+                className={`text-sm w-1/6 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'shop' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
                 onClick={() => setCurrentPage('shop')}
             >
-                <Image src={currentPage === 'shop' ? '/icons/Shop-black.svg' : '/icons/Shop-white.svg'} alt='shop' height={20} width={20}/>
+                <Image src={currentPage === 'shop' ? '/icons/Shop-black.svg' : '/icons/Shop-white.svg'} alt='shop' height={20} width={20} />
                 <p className='text-xs font-bold'>Shop</p>
             </button>
             <button
-                className={`text-sm px-2 ${currentPage === 'page2' ? 'text-yellow-400 rounded-xl' : 'text-white'}`}
-                onClick={() => setCurrentPage('page2')}
+                className={`text-sm w-1/6 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'play' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
+                onClick={() => setCurrentPage('play')}
             >
-                Page 2
+                <Image src={currentPage === 'play' ? '/icons/Football-black.svg' : '/icons/Football-white.svg'} alt='play' height={20} width={20} />
+                <p className='text-xs font-bold'>Play</p>
             </button>
             <button
-                className={`text-sm px-2 ${currentPage === 'page1' ? 'text-yellow-400 rounded-xl' : 'text-white'}`}
-                onClick={() => setCurrentPage('page1')}
+                className={`text-sm w-1/6 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'earn' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
+                onClick={() => setCurrentPage('earn')}
             >
-                Page 1
+                <Image src={currentPage === 'earn' ? '/icons/Money-black.svg' : '/icons/Money-white.svg'} alt='earn' height={20} width={20}/>
+                <p className='text-xs font-bold'>Earn</p>
             </button>
             <button
-                className={`text-sm px-2 ${currentPage === 'page2' ? 'text-yellow-400 rounded-xl' : 'text-white'}`}
-                onClick={() => setCurrentPage('page2')}
+                className={`text-sm w-1/6 py-1 flex flex-col justify-center items-center gap-1 ${currentPage === 'lineup' ? 'bg-yellow-400 rounded-xl text-black' : 'text-white'}`}
+                onClick={() => setCurrentPage('lineup')}
             >
-                Page 2
+                <Image src={currentPage === 'lineup' ? '/icons/Lineup-black.svg' : '/icons/Lineup-white.svg'} alt='lineup' height={20} width={20}/>
+                <p className='text-xs font-bold'>Lineup</p>
             </button>
         </nav>
     );
