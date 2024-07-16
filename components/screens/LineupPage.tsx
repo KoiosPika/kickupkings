85eb6e79510df39a1a38a7bb98302715a051e7a7
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-15 21:46:58 -0700

    display formations

--- components/screens/LineupPage.tsx
+++ components/screens/LineupPage.tsx
@@ -1,6 +1,7 @@
 import { formations } from '@/constants/Formations';
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
+import { ScrollArea, ScrollBar } from '../ui/scroll-area';
 
 const colors = [
   { 'Forward': '#EE2E0C' },
@@ -24,9 +25,12 @@ const LineupPage = () => {
 
   const currentFormation = formations.find(f => f.id === selectedFormation);
 
-  const getColor = (type: any) => {
+  const getColor = (type: any, position: any) => {
+    if (!position) {
+      return '';
+    }
     const colorObj: any = colors.find((color: any) => color[type]);
-    return colorObj ? colorObj[type] : '#000';
+    return colorObj ? colorObj[type] : '';
   };
 
 
@@ -38,11 +42,11 @@ const LineupPage = () => {
         <p className='font-semibold text-white text-[13px]'>{`->`}</p>
       </div>
       <div style={{ height: height - 220 }} className='relative'>
-        <div className='h-full w-full absolute flex flex-col justify-around'>
+        <div className='h-full w-full absolute flex flex-col justify-evenly'>
           {currentFormation?.data.map((row, rowIndex) => (
             <div key={rowIndex} className='flex justify-around'>
               {row.positions.map((position, posIndex) => (
-                <div key={posIndex} className='p-2 rounded-lg text-white font-semibold' style={{ backgroundColor: getColor(row.type) }}>
+                <div key={posIndex} className='p-2 rounded-lg text-white font-semibold h-1/8' style={{ backgroundColor: getColor(row.type, row.positions[posIndex]), height: row.type === 'Spacer' ? '50%' : '100%' }}>
                   {position}
                 </div>
               ))}
@@ -51,21 +55,25 @@ const LineupPage = () => {
         </div>
         <Image src={'/Field.png'} alt='field' height={500} width={500} style={{ height: height - 220 }} />
       </div>
-      <div className='p-4'>
-        <label htmlFor='formation-select' className='mr-2'>Select Formation:</label>
-        <select
-          id='formation-select'
-          value={selectedFormation}
-          onChange={(e) => setSelectedFormation(e.target.value)}
-          className='p-2 border rounded'
-        >
+      <ScrollArea style={{ height: height - 560 }} className='py-2 px-2'>
+        <div className='flex gap-2 overflow-x-auto'>
           {formations.map((formation) => (
-            <option key={formation.id} value={formation.id}>
-              {formation.id}
-            </option>
+            <div key={formation.id} className='flex flex-col h-full justify-center items-center border-[1px] border-white rounded-lg' onClick={() => setSelectedFormation(formation.id)}>
+              <div className='bg-slate-600 rounded-t-lg flex flex-col justify-evenly' style={{ height: height - 600, aspectRatio: 5 / 4 }}>
+                {formation?.data.map((row, rowIndex) => (
+                  <div key={rowIndex} className='flex justify-around py-[2px]'>
+                    {row.positions.map((position, posIndex) => (
+                      <div key={posIndex} className='p-[2px] rounded-lg text-white font-semibold' style={{ backgroundColor: getColor(row.type, row.positions[posIndex]), height: row.type === 'Spacer' ? '50%' : '100%' }} />
+                    ))}
+                  </div>
+                ))}
+              </div>
+              <div className='w-full text-center bg-white rounded-b-md'>{formation.id}</div>
+            </div>
           ))}
-        </select>
-      </div>
+        </div>
+        <ScrollBar orientation="horizontal" className='hidden' />
+      </ScrollArea>
     </section>
   )
 }
--- constants/Formations.ts
+++ constants/Formations.ts
@@ -1,13 +1,13 @@
 export const formations = [
     {
-      id: '4-2-3-1',
+        id: '4-4-2',
       data: [
-        { positions: ['CF'], type: 'Forward' },
-        { positions: [], type: 'Spacer' },
-        { positions: ['LWM', 'CAM', 'RWM'], type: 'Midfield' },
-        { positions: [], type: 'Spacer' },
-        { positions: ['LDM', 'RDM'], type: 'Midfield' },
-        { positions: [], type: 'Spacer' },
+        { positions: ['RST', 'LST'], type: 'Forward' },
+        { positions: [''], type: 'Spacer' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LM','LCM', 'RCM','RM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: [''], type: 'Spacer' },
         { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
         { positions: ['GK'], type: 'Goalkeeper' }
       ]
@@ -16,10 +16,101 @@ export const formations = [
       id: '4-1-2-1-2',
       data: [
         { positions: ['RST', 'LST'], type: 'Forward' },
-        { positions: [], type: 'Spacer' },
+        { positions: [''], type: 'Spacer' },
         { positions: ['CAM'], type: 'Midfield' },
         { positions: ['LCM', 'RCM'], type: 'Midfield' },
         { positions: ['CDM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-3-1-2',
+      data: [
+        { positions: ['RST', 'LST'], type: 'Forward' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['CAM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['RDM','CDM','LDM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-2-2-2',
+      data: [
+        { positions: ['RST', 'LST'], type: 'Forward' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LWM','','RWM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['RDM','LDM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-4-1-1',
+      data: [
+        { positions: ['ST'], type: 'Forward' },
+        { positions: ['CF'], type: 'Forward' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LM','RCM','LCM','RM'], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-1-3-2',
+      data: [
+        { positions: ['LST','RST'], type: 'Forward' },
+        { positions: [''], type: 'Forward' },
+        { positions: ['LWM','CAM','RWM'], type: 'Midfield' },
+        { positions: [''], type: 'Midfield' },
+        { positions: ['','CDM',''], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-5-1',
+      data: [
+        { positions: [''], type: 'Forward' },
+        { positions: ['CF'], type: 'Forward' },
+        { positions: [''], type: 'Midfield' },
+        { positions: ['LM','','CM','','RM'], type: 'Midfield' },
+        { positions: ['','LDM','','RDM',''], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-1-4-1',
+      data: [
+        { positions: ['CF'], type: 'Forward' },
+        { positions: [''], type: 'Forward' },
+        { positions: ['LWM','LAM','RAM','RWM'], type: 'Midfield' },
+        { positions: [''], type: 'Midfield' },
+        { positions: ['','','CDM','',''], type: 'Midfield' },
+        { positions: [''], type: 'Spacer' },
+        { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
+        { positions: ['GK'], type: 'Goalkeeper' }
+      ]
+    },
+    {
+      id: '4-2-3-1',
+      data: [
+        { positions: ['CF'], type: 'Forward' },
+        { positions: [], type: 'Spacer' },
+        { positions: ['LWM', 'CAM', 'RWM'], type: 'Midfield' },
+        { positions: [], type: 'Spacer' },
+        { positions: ['LDM', 'RDM'], type: 'Midfield' },
         { positions: [], type: 'Spacer' },
         { positions: ['LB', 'LCB', 'RCB', 'RB'], type: 'Defense' },
         { positions: ['GK'], type: 'Goalkeeper' }
