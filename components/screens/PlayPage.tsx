d33907e0beb7613748348b6fdc6257b1a27b5c7f
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-17 15:21:58 -0700

    playpage ui

--- components/screens/PlayPage.tsx
+++ components/screens/PlayPage.tsx
@@ -47,11 +47,11 @@ const PlayPage = () => {
               <p className='text-[16px] sm:text-[20px]'>Form</p>
             </div>
             <div className='w-full bg-slate-800 text-white text-center font-semibold rounded-bl-lg flex flex-row items-center justify-center gap-1 h-1/2'>
-              <p className='rounded-sm bg-green-500 w-1/6 text-[13px] sm:text-[18px]'>W</p>
-              <p className='rounded-sm bg-red-500 w-1/6   text-[13px] sm:text-[18px]'>L</p>
-              <p className='rounded-sm bg-green-500 w-1/6 text-[13px] sm:text-[18px]'>W</p>
-              <p className='rounded-sm bg-green-500 w-1/6 text-[13px] sm:text-[18px]'>W</p>
-              <p className='rounded-sm bg-red-500 w-1/6   text-[13px] sm:text-[18px]'>L</p>
+              <p className='rounded-sm bg-green-600 w-1/6 text-[13px] sm:text-[18px]'>W</p>
+              <p className='rounded-sm bg-red-600 w-1/6   text-[13px] sm:text-[18px]'>L</p>
+              <p className='rounded-sm bg-green-600 w-1/6 text-[13px] sm:text-[18px]'>W</p>
+              <p className='rounded-sm bg-green-600 w-1/6 text-[13px] sm:text-[18px]'>W</p>
+              <p className='rounded-sm bg-red-600 w-1/6   text-[13px] sm:text-[18px]'>L</p>
             </div>
           </div>
           <div className='w-1/5 flex flex-col h-[60px] sm:h-[80px] gap-[3px]'>
@@ -82,41 +82,41 @@ const PlayPage = () => {
       </div>
       <div className='w-full flex flex-col justify-center items-center mt-2'>
         <div className='w-11/12'>
-          <p className='text-white font-semibold bg-slate-800 px-2 py-1 inline-flex rounded-lg'>History</p>
-          <div className='flex flex-col gap-1 my-2'>
-            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center border-[1px] border-white'>
-              <p className='h-[25px] w-[30px] text-center bg-red-500 rounded-sm'>L</p>
-              <p className='ml-2'>4-1</p>
+          <p className='text-white font-semibold bg-slate-800 px-3 py-1 inline-flex rounded-lg text-[16px] sm:text-[22px]'>History</p>
+          <div className='flex flex-col gap-1 sm:gap-4 my-2'>
+            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center gap-1 sm:gap-5 border-[1px] border-white'>
+              <p className='h-[25px] w-[30px] sm:h-[45px] sm:w-[50px] text-[16px] sm:text-[30px] text-center bg-red-600 rounded-sm'>L</p>
+              <p className='ml-2 text-[16px] sm:text-[30px]'>4-1</p>
               <div className='ml-5 flex flex-row items-center bg-slate-900 px-2 py-1 rounded-lg'>
-                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] rounded-lg' />
-                <p className='ml-2 max-w-[100px] overflow-hidden'>username</p>
+                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] sm:h-[48px] sm:w-[48px] rounded-lg' />
+                <p className='ml-2 max-w-[80px] sm:max-w-[200px] text-[16px] sm:text-[24px] overflow-hidden'>username</p>
               </div>
-              <p className='bg-purple-700 px-2 text-[14px] py-[2px] rounded-lg ml-auto shadow-md shadow-purple-500'>Rematch</p>
+              <p className='bg-purple-700 px-2 text-[14px] sm:text-[24px] py-[2px] rounded-lg ml-auto shadow-md shadow-purple-500 border-b-[3px] sm:border-b-[6px] border-purple-800'>Rematch</p>
             </div>
-            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center border-[1px] border-white'>
-              <p className='h-[25px] w-[30px] text-center bg-green-500 rounded-sm'>W</p>
-              <p className='ml-2'>4-2</p>
+            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center gap-1 sm:gap-5 border-[1px] border-white'>
+              <p className='h-[25px] w-[30px] sm:h-[45px] sm:w-[50px] text-[16px] sm:text-[30px] text-center bg-green-600 rounded-sm'>W</p>
+              <p className='ml-2 text-[16px] sm:text-[30px]'>4-2</p>
               <div className='ml-5 flex flex-row items-center bg-slate-900 px-2 py-1 rounded-lg'>
-                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] rounded-lg' />
-                <p className='ml-2 max-w-[100px] overflow-hidden'>username</p>
+                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] sm:h-[48px] sm:w-[48px] rounded-lg' />
+                <p className='ml-2 max-w-[80px] sm:max-w-[200px] text-[16px] sm:text-[24px] overflow-hidden'>username</p>
               </div>
-              <p className='bg-purple-700 px-2 text-[14px] py-[2px] rounded-lg ml-auto shadow-md shadow-purple-500'>Rematch</p>
+              <p className='bg-purple-700 px-2 text-[14px] sm:text-[24px] py-[2px] rounded-lg ml-auto shadow-md shadow-purple-500 border-b-[3px] sm:border-b-[6px] border-purple-800'>Rematch</p>
             </div>
-            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center border-[1px] border-white'>
-              <p className='h-[25px] w-[30px] text-center bg-red-500 rounded-sm'>L</p>
-              <p className='ml-2'>5-3</p>
+            <div className='text-white font-semibold bg-slate-800 p-2 rounded-lg flex flex-row items-center gap-1 sm:gap-5 border-[1px] border-white'>
+              <p className='h-[25px] w-[30px] sm:h-[45px] sm:w-[50px] text-[16px] sm:text-[30px] text-center bg-red-600 rounded-sm'>L</p>
+              <p className='ml-2 text-[16px] sm:text-[30px]'>5-3</p>
               <div className='ml-5 flex flex-row items-center bg-slate-900 px-2 py-1 rounded-lg'>
-                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] rounded-lg' />
-                <p className='ml-2 max-w-[100px] overflow-hidden'>username</p>
+                <Image src={'/icons/user.svg'} alt='user' height={50} width={50} className='bg-slate-500 p-1 h-[28px] w-[28px] sm:h-[48px] sm:w-[48px] rounded-lg' />
+                <p className='ml-2 max-w-[80px] sm:max-w-[200px] text-[16px] sm:text-[24px] overflow-hidden'>username</p>
               </div>
-              <p className='bg-purple-500 px-2 text-[14px] py-[2px] rounded-lg ml-auto shadow-md'>Rematched</p>
+              <p className='bg-purple-500 px-2 text-[14px] sm:text-[24px] py-[2px] rounded-lg ml-auto shadow-md shadow-purple-500 border-b-[3px] sm:border-b-[6px] border-purple-600'>Rematched</p>
             </div>
           </div>
         </div>
       </div>
       <div className='w-full flex flex-col justify-center items-center mt-2'>
-        <div className='bg-blue-500 px-3 py-2 font-semibold text-white rounded-xl shadow-blue-600 shadow-lg border-b-[4px] border-blue-800'>
-          <p className='text-[22px]'>Find Match</p>
+        <div className='bg-blue-500 px-3 py-2 font-semibold text-white rounded-xl shadow-blue-600 shadow-lg border-b-[4px] sm:border-b-[8px] border-blue-800'>
+          <p className='text-[18px] sm:text-[34px] px-4'>Find Match</p>
         </div>
       </div>
       <div className='w-full flex flex-col h-full justify-center items-center flex-grow mt-2'>
