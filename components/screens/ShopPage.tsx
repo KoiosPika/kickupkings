6dee62ae2f3f85a7bc10ff2e4bbac6c841e49ab4
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-17 11:33:33 -0700

    shop drawer

--- components/screens/ShopPage.tsx
+++ components/screens/ShopPage.tsx
@@ -2,6 +2,16 @@ import { positions } from '@/constants';
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { ScrollArea } from '../ui/scroll-area';
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
 
 const ShopPage = () => {
 
@@ -66,23 +76,45 @@ const ShopPage = () => {
                         {positions
                             .filter((position) => position.type === selectedType)
                             .map((position: any, index: number) => (
-                                <div key={index} className='flex flex-col justify-center items-center w-full bg-slate-800 rounded-xl h-[100px] sm:h-[150px] shadow-slate-200 shadow-sm border-[1px] border-slate-300'>
-                                    <div className='flex flex-row items-center w-full p-2 gap-1'>
-                                        <div className={`p-2 rounded-md font-bold w-1/4 sm:w-1/3 text-center text-white h-full flex justify-center items-center text-[13px] sm:text-[25px] border-2 border-white`} style={{ backgroundColor: position.color, boxShadow: `-8px -8px 10px -6px ${position.color},-8px 8px 10px -6px ${position.color},8px -8px 10px -6px ${position.color},8px 8px 10px -6px ${position.color}` }}>
-                                            <p>{position.symbol}</p>
-                                        </div>
-                                        <div className='flex flex-col justify-center items-center w-3/4 sm:w-2/3 gap-1'>
-                                            <p className='text-[11px] sm:text-[18.5px] font-bold text-white'>{position.label}</p>
-                                            <div className='flex flex-row items-center gap-2 bg-slate-600 px-2 py-[2px] sm:py-[5px] rounded-lg'>
-                                                <Image src={'/icons/coin.svg'} alt='coin' height={100} width={100} className='w-[20px] h-[20px] sm:w-[35px] sm:h-[35px]' />
-                                                <p className='font-semibold text-white text-[16px] sm:text-[25px]'>50</p>
+                                <Drawer key={index}>
+                                    <DrawerTrigger>
+                                        <div className='flex flex-col justify-center items-center w-full bg-slate-800 rounded-xl h-[100px] sm:h-[150px] shadow-slate-200 shadow-sm border-[1px] border-slate-300'>
+                                            <div className='flex flex-row items-center w-full p-2 gap-1'>
+                                                <div className={`p-2 rounded-md font-bold w-1/4 sm:w-1/3 text-center text-white h-full flex justify-center items-center text-[13px] sm:text-[25px] border-2 border-white`} style={{ backgroundColor: position.color, boxShadow: `-8px -8px 10px -6px ${position.color},-8px 8px 10px -6px ${position.color},8px -8px 10px -6px ${position.color},8px 8px 10px -6px ${position.color}` }}>
+                                                    <p>{position.symbol}</p>
+                                                </div>
+                                                <div className='flex flex-col justify-center items-center w-3/4 sm:w-2/3 gap-1'>
+                                                    <p className='text-[11px] sm:text-[18.5px] font-bold text-white'>{position.label}</p>
+                                                    <div className='flex flex-row items-center gap-2 bg-slate-600 px-2 py-[2px] sm:py-[5px] rounded-lg'>
+                                                        <Image src={'/icons/coin.svg'} alt='coin' height={100} width={100} className='w-[20px] h-[20px] sm:w-[35px] sm:h-[35px]' />
+                                                        <p className='font-semibold text-white text-[16px] sm:text-[25px]'>50</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className='border-t-2 border-slate-300 w-full py-1 sm:py-2'>
+                                                <p className='font-semibold text-white text-center text-[14px] sm:text-[25px]'>Level 0 {`->`} Level 1</p>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className='border-t-2 border-slate-300 w-full py-1 sm:py-2'>
-                                        <p className='font-semibold text-white text-center text-[14px] sm:text-[25px]'>Level 0 {`->`} Level 1</p>
-                                    </div>
-                                </div>))}
+                                    </DrawerTrigger>
+                                    <DrawerContent className=' h-[50%] sm:h-[40%] border-t-8 border-x-0 border-yellow-400 bg-slate-800'>
+                                        <DrawerHeader>
+                                            <DrawerTitle className='text-white my-3'>Upgrade {position.label} to Level 3?</DrawerTitle>
+                                            <div className={`p-2 rounded-md font-bold w-1/5 place-self-center text-center text-white h-[70px] flex justify-center items-center text-[23px] border-2 border-white`} style={{ backgroundColor: position.color, boxShadow: `-8px -8px 10px -6px ${position.color},-8px 8px 10px -6px ${position.color},8px -8px 10px -6px ${position.color},8px 8px 10px -6px ${position.color}` }}>
+                                                <p>{position.symbol}</p>
+                                            </div>
+                                        </DrawerHeader>
+                                        <div className='flex flex-row items-center justify-center gap-2 bg-slate-600 px-2 py-[2px] sm:py-[5px] rounded-xl w-2/5 place-self-center'>
+                                            <Image src={'/icons/coin.svg'} alt='coin' height={100} width={100} className='w-[30px] h-[30px]' />
+                                            <p className='font-semibold text-white text-[25px]'>50</p>
+                                        </div>
+                                        <DrawerFooter className='mb-5'>
+                                            <div style={{ backgroundColor: position.color }} className='w-3/4 py-2 rounded-lg text-white font-bold text-center place-self-center'>Upgrade now</div>
+                                            <DrawerClose className='flex justify-center items-center'>
+                                                <div className='bg-white text-black font-bold py-2 rounded-lg w-3/4'>Cancel</div>
+                                            </DrawerClose>
+                                        </DrawerFooter>
+                                    </DrawerContent>
+                                </Drawer>))}
                     </div>
                 </ScrollArea>
             </div>
