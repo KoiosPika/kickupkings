d315e3bdcb52bd21bfc94c3f4241fb957ab54e24
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-19 23:24:42 -0700

    spin drawer

--- components/screens/ShopPage.tsx
+++ components/screens/ShopPage.tsx
@@ -87,12 +87,49 @@ const ShopPage = () => {
                         <p className='font-bold text-white text-[16px] sm:text-[22px]'>{user && user?.diamonds}</p>
                     </div>
                 </div>
-                <div className='w-1/3 bg-slate-800 flex flex-row justify-around items-center rounded-lg h-[53px] sm:h-[75px]'>
-                    <div className='flex flex-row items-center gap-1'>
-                        <Image src={'/icons/dice.svg'} alt='coin' height={100} width={100} className='w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]' />
-                        <p className='font-bold text-white text-[16px] sm:text-[22px]'>Spin</p>
-                    </div>
-                </div>
+                <Drawer>
+                    <DrawerTrigger className='w-1/3 bg-slate-800 flex flex-row justify-around items-center rounded-lg h-[53px] sm:h-[75px]'>
+                        <div className='flex flex-row items-center gap-1'>
+                            <Image src={'/icons/dice.svg'} alt='coin' height={100} width={100} className='w-[50px] h-[50px] sm:w-[70px] sm:h-[70px]' />
+                            <p className='font-bold text-white text-[16px] sm:text-[22px]'>Spin</p>
+                        </div>
+                    </DrawerTrigger>
+                    <DrawerContent className={` h-[80%] sm:h-[40%] border-t-8 border-orange-500 border-x-0 bg-slate-800`}>
+                        <DrawerHeader>
+                            <DrawerTitle className='text-white my-3'>Lucky Spin</DrawerTitle>
+                        </DrawerHeader>
+                        <div className='flex flex-col justify-center items-center gap-3'>
+                            <div className='flex flex-row justify-center items-center gap-7'>
+                                <div className={`p-2 rounded-md font-bold w-[60px] h-[60px] text-center text-white flex justify-center items-center text-[18px] sm:text-[25px] border-2 border-white relative rotate-[340deg]`} style={{ backgroundColor: 'green', boxShadow: `-8px -8px 10px -6px green,-8px 8px 10px -6px green,8px -8px 10px -6px green,8px 8px 10px -6px green` }}>
+                                    <p>GK</p>
+                                    <p className='absolute bottom-0 right-1 text-[14px]'>+1</p>
+                                </div>
+                                <div className='flex flex-col justify-center items-center gap-3'>
+                                    <Image src={'/icons/coin.svg'} alt='coin' height={50} width={50} />
+                                    <p className='bg-slate-600 text-white font-semibold px-2 rounded-full'>+ 500</p>
+                                </div>
+                                <div className={`p-2 rounded-md font-bold w-[60px] h-[60px] text-center text-white flex justify-center items-center text-[18px] sm:text-[25px] border-2 border-white relative rotate-[20deg]`} style={{ backgroundColor: 'red', boxShadow: `-8px -8px 10px -6px green,-8px 8px 10px -6px red,8px -8px 10px -6px red,8px 8px 10px -6px red` }}>
+                                    <p>ST</p>
+                                    <p className='absolute bottom-0 right-1 text-[14px]'>+2</p>
+                                </div>
+                            </div>
+                            <p className='w-10/12 text-center text-white font-semibold'>You can win a level upgrade or coins from every spin</p>
+                        </div>
+                        <div className='flex justify-center items-center mt-6 relative'>
+                            <div className='h-[200px] w-[200px] rounded-full bg-gradient-to-t from-green-700 to-red-500 flex justify-center items-center animate-spin' style={{ boxShadow: `-8px -8px 10px -6px red,-8px 8px 10px -6px green,8px -8px 10px -6px green,8px 8px 10px -6px red` }} />
+                            <div className='h-[180px] w-[180px] bg-slate-700 rounded-full flex flex-col gap-2 justify-center items-center absolute'>
+                                <p className='rounded-full text-white mr-2 text-[24px] font-semibold'>Spin</p>
+                                <div className='flex flex-row justify-center items-center bg-slate-600 text-white font-semibold gap-1 px-2 py-1 rounded-full'>
+                                    <p className='text-[18px]'>5</p>
+                                    <Image src={'/icons/diamond.svg'} alt='coin' height={100} width={100} className='w-[25px] h-[25px] sm:w-[40px] sm:h-[40px]' />
+                                </div>
+                            </div>
+                        </div>
+                        <DrawerClose className='absolute text-white right-4 top-4'>
+                            <Image src={'/icons/x.svg'} alt='coin' height={100} width={100} className='w-[25px] h-[25px] sm:w-[40px] sm:h-[40px]' />
+                        </DrawerClose>
+                    </DrawerContent>
+                </Drawer>
             </div>
             <div className='w-full flex flex-col justify-center items-center my-2'>
                 <div className='w-11/12 bg-slate-800 flex flex-row items-center justify-evenly px-1 py-1 rounded-lg my-2'>
@@ -124,7 +161,7 @@ const ShopPage = () => {
                                                     <p className='text-[11px] sm:text-[18.5px] font-bold text-white'>{position.label}</p>
                                                     <div className='flex flex-row items-center gap-2 bg-slate-600 px-2 py-[2px] sm:py-[5px] rounded-lg'>
                                                         <Image src={'/icons/coin.svg'} alt='coin' height={100} width={100} className='w-[20px] h-[20px] sm:w-[35px] sm:h-[35px]' />
-                                                        {user && <p className='font-semibold text-white text-[16px] sm:text-[25px]' style={{color:user?.coins >= price ? 'white' : 'red'}}>{price}</p>}
+                                                        {user && <p className='font-semibold text-white text-[16px] sm:text-[25px]' style={{ color: user?.coins >= price ? 'white' : 'red' }}>{price}</p>}
                                                     </div>
                                                 </div>
                                             </div>
@@ -142,7 +179,7 @@ const ShopPage = () => {
                                         </DrawerHeader>
                                         <div className='flex flex-row items-center justify-center gap-2 bg-slate-600 px-2 py-[2px] sm:py-[5px] rounded-xl w-2/5 place-self-center'>
                                             <Image src={'/icons/coin.svg'} alt='coin' height={100} width={100} className='w-[30px] h-[30px]' />
-                                            {user && <p className='font-semibold text-white text-[25px]' style={{color:user?.coins >= price ? 'white' : 'red'}}>{price}</p>}
+                                            {user && <p className='font-semibold text-white text-[25px]' style={{ color: user?.coins >= price ? 'white' : 'red' }}>{price}</p>}
                                         </div>
                                         <DrawerFooter className='mb-5'>
                                             <div style={{ backgroundColor: position.color }} className='w-3/4 py-2 rounded-lg text-white font-bold text-center place-self-center' onClick={() => handleUpgrade(position.symbol)}>{upgrading ? 'Upgrading...' : 'Upgrade now'}</div>
