8fccca700c47b435cfc513d9de6c88be4a33b28a
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-24 16:45:31 -0700

    telegram bot

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -11,31 +11,33 @@ export async function POST(request: any) {
         return NextResponse.json({ status: 'error', message: 'Unauthorized' }, { status: 401 });
     }
 
-    try {
-        const { telegramId, amount } = await request.json();
+    // try {
+    //     const { telegramId, amount } = await request.json();
 
-        // Find the user by their Telegram ID
-        let user = await User.findOne({ telegramID: telegramId });
+    //     // Find the user by their Telegram ID
+    //     let user = await User.findOne({ telegramID: telegramId });
 
-        if (!user) {
-            throw new Error('User not found');
-        }
+    //     if (!user) {
+    //         throw new Error('User not found');
+    //     }
 
-        let userData = await UserData.findOne({ User: user._id })
+    //     let userData = await UserData.findOne({ User: user._id })
 
-        if (!userData) {
-            throw new Error('User data not found');
-        }
+    //     if (!userData) {
+    //         throw new Error('User data not found');
+    //     }
 
-        // Update the user's diamond count
-        userData.diamonds += amount;
+    //     // Update the user's diamond count
+    //     userData.diamonds += amount;
 
-        // Save the user's updated information
-        await userData.save();
+    //     // Save the user's updated information
+    //     await userData.save();
 
-        return NextResponse.json({ status: 'success', diamonds: user.diamonds });
-    } catch (error) {
-        console.error('Error updating diamonds:', error);
-        return NextResponse.json({ status: 'error', message: 'Failed to update diamonds' }, { status: 500 });
-    }
+    //     return NextResponse.json({ status: 'success', diamonds: user.diamonds });
+    // } catch (error) {
+    //     console.error('Error updating diamonds:', error);
+    //     return NextResponse.json({ status: 'error', message: 'Failed to update diamonds' }, { status: 500 });
+    // }
+
+    return NextResponse.json({ status: 'success', message: 'Endpoint working' });
 }
