9f55af74ef886b874cc6c7df5f06ce67f7a8470a
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-08-03 21:47:35 -0700

    added icons

--- components/screens/PlayPage.tsx
+++ components/screens/PlayPage.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { ScrollArea } from '../ui/scroll-area'
 import { formations } from '@/constants/Formations'
 import { positions } from '@/constants'
-import { addChatID, createFakeUsers, createUser, findMatch, getUserByUserID, getUserForPlayPage, playGame } from '@/lib/actions/user.actions'
+import { createFakeUsers, createUser, findMatch, getUserByUserID, getUserForPlayPage, playGame } from '@/lib/actions/user.actions'
 import { IUserData } from '@/lib/database/models/userData.model'
 import {
   AlertDialog,
