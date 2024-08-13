e3551f27c896e09a6b356d583191e5ee2ed5f221
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-08-12 17:29:20 -0700

    removed unique username

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -10,7 +10,7 @@ export interface IUser extends Document {
 const UserSchema = new Schema({
     telegramID: { type: String, required: true, unique: true },
     chatId: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     bio: { type: String, default: '' },
     photo: { type: String },
     type: { type: String, default: 'User' },
